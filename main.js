cd02ca4b3a432bd164813fde894e52422b697f13
Yifei Wang <hello.yifei@icloud.com>
2017-03-30 00:05:35 +0200

    added function for sorting

--- main.js
+++ main.js
@@ -1,44 +1,53 @@
 var table = document.getElementById("table");
-function sortByPrice(){
 
+function submit(){
   let price = document.getElementById("price");
   let fb = firebase.database();
-  fb.ref().child('product/').once('value')
-    .then(function(snapshot){
-      let data = snapshot.val();
-      if(data === null) {
-        data={}
-      }
-      let product = {
-        name: "dfdf",
-        price: 23,
-      }
-      return fb.ref('product/').push(product);
-    })
-    .then(function(){
-      let table = document.getElementById("table");
-      table.innerHTML = '';
-      return listMessages();
-    })
-    .catch(function(err){
-    })
+  var productName= document.getElementById("name");
+  var productPrice = document.getElementById("productPrice");
+  var product = {
+    name: productName.value,
+    price: Number(productPrice.value)
+  }
+  fb.ref('products/').push(product);
 }
-function listMessages(){
-  let table = document.getElementById("table");
-  table.innerHTML = '';
-  firebase.database().ref('product/').once('value')
+//orderBy('name')
+function orderBy(key){
+  var outputNumber = document.getElementById("outputNumber");
+  let fb = firebase.database();
+//do new stuff, put debugger before and then check on console
+  fb.ref('products/').orderByChild(`${key}`).limitToFirst(Number(outputNumber.value)).once('value')//get once all values, read all orderByChild("name")
   .then(function(snapshot){
-      let data = snapshot.val();
-      if(data === null){
-        data={}
-      }
-      Object.keys(data).reverse().forEach(function(key){
-         let pro = data[key];
-        let tr = document.createElement('tr');
-        tr.innerHTML = `<td>${pro.name}</td>
-        <td>${pro.price}</td>`;
-        table.appendChild(tr);
-      })
+    //after snapshot that get all values then can clean
+    let table = document.getElementById("table");
+    table.innerHTML = "";
+    snapshot.forEach(function(child){
+      let tr = document.createElement("tr");//child is a object, child.key to get id
+      tr.innerHTML = `<td>${child.val().name}</td>
+      <td>${child.val().price}</td>`
+      table.appendChild(tr);
+      // console.log(child.val())//object child/hela object  child.val  object
     })
-
+  })
+  .catch(function(err){
+  })
 }
+// function orderByPrice(){
+//   let fb = firebase.database();
+//     fb.ref('products/').orderByChild('price').once('value')//get once all values, read all
+//       .then(function(snapshot){
+//         let table = document.getElementById("table");
+//         table.innerHTML = "";
+//         snapshot.forEach(function(child){
+//
+//           let tr = document.createElement("tr");//child is a object, child.key to get id
+//           tr.innerHTML = `<td>${child.val().name}</td>
+//           <td>${child.val().price}</td>`
+//           table.appendChild(tr);
+//           // console.log(child.val())//object child/hela object  child.val  object
+//         })
+//       })
+//
+//       .catch(function(err){
+//       })
+//}
